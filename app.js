63894cd9db7ed39d38df962905d74be8d8419884
Quaxier <mjaudwo1@gmail.com>
2023-12-20 16:55:21 +0900

    app.js committed

--- app.js
+++ app.js
@@ -13,6 +13,34 @@ app.get('/',(req, res) => {
   res.sendFile(path.join(__dirname + '/html/index.html'));
   })
 
+app.get('/index.html',(req, res) => {
+  res.sendFile(path.join(__dirname + '/html/index.html'));
+  })
+
+app.get('/cartlist.html',(req, res) => {
+  res.sendFile(path.join(__dirname + '/html/cartlist.html'));
+  })
+
+app.get('/wishlist.html',(req, res) => {
+  res.sendFile(path.join(__dirname + '/html/wishlist.html'));
+  })
+
+app.get('/mypage.html',(req, res) => {
+    res.sendFile(path.join(__dirname + '/html/mypage.html'));
+  })
+  
+app.get('/signin.html',(req, res) => {
+  res.sendFile(path.join(__dirname + '/html/signin.html'));
+  })
+  
+app.get('/login.html',(req, res) => {
+  res.sendFile(path.join(__dirname + '/html/login.html'));
+  })
+
+app.get('/gameinfo.html',(req, res) => {
+  res.sendFile(path.join(__dirname + '/html/gameinfo.html'));
+  })
+  
 app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기중');
 });
